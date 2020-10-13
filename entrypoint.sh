231533bd7ee3df9280316c703252f65fe7cbb3b5
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 20:55:26 +0200

    Fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,24 @@
 #!/bin/sh -l
 
-if [ -z "$GITHUB_TOKEN" ]
+if [ -z "$1" ]
 then
       echo "\$GITHUB_TOKEN is empty"
 else
       echo "\$GITHUB_TOKEN is NOT empty"
 fi
 
-if [ -z "$PA_TOKEN" ]
+if [ -z "$2" ]
 then
       echo "\$PA_TOKEN is empty"
 else
       echo "\$PA_TOKEN is NOT empty"
 fi
 
-if [ -z "$REPOSITORY" ]
+if [ -z "$3" ]
 then
       echo "\$REPOSITORY is empty"
 else
-      echo "\$REPOSITORY is NOT empty"
+      echo "\$REPOSITORY is NOT empty: $3"
 fi
 
 exit 0
