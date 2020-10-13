f8a04273870bbd54c4d806dbe75802e67d26f236
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 20:46:34 +0200

    Changed variable names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/sh -l
 
-echo "test"
+if [ -z "$GITHUB_TOKEN" ]
+then
+      echo "\$GITHUB_TOKEN is empty"
+else
+      echo "\$GITHUB_TOKEN is NOT empty"
+fi
+
+if [ -z "$PA_TOKEN" ]
+then
+      echo "\$PA_TOKEN is empty"
+else
+      echo "\$PA_TOKEN is NOT empty"
+fi
+
+if [ -z "$REPOSITORY" ]
+then
+      echo "\$REPOSITORY is empty"
+else
+      echo "\$REPOSITORY is NOT empty"
+fi
 
 exit 0
