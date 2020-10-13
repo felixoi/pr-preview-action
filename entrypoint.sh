7759a6bcd6435ed2cd4c90c94cc5e15be3c5d8fb
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 22:05:52 +0200

    Another fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,6 @@ result=$(curl -H "Authorization: token $1" -H "Accept: application/vnd.github.v3
 curl -H "Authorization: token $1" -H "Accept: application/vnd.github.v3.full+json" \
  https://api.github.com/repos/"$3"/pulls/"$5"
 
+echo "https://api.github.com/repos/$3/pulls/$5"
+
 echo "$result" | jq '.head.ref'
