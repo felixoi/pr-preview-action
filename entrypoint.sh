20c19ffba0f1e16b4c5c2b5bc47c44217e53d66f
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 23:30:40 +0200

    Another fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,22 +11,24 @@ result2=$(curl \
   -H "Authorization: token $1" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/"$3"/deployments \
-  -d "{'ref':'$branch', environment: 'dev', required_contexts: []}")
+  -d "{\"ref\":\"$branch\", \"environment\":\"dev\", \"required_contexts\": [], \"auto_merge\": false}")
 
 deployment_id=$(echo "$result2" | jq '.id')
 
 curl \
   -X POST \
-  -H "Content-Type: application/json,application/vnd.github.ant-man-preview+json,application/vnd.github.flash-preview+json" \
+  -H "Content-Type: application/json" \
   -H "Authorization: token $1" \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github.v3+json,application/vnd.github.ant-man-preview+json,application/vnd.github.flash-preview+json" \
   https://api.github.com/repos/"$3"/deployments/"$deployment_id"/statuses \
-  -d "{environment: 'dev', environment_url: 'http://example.com', state: 'in_progress', log_url: 'https://github.com/$3/actions/runs/$6'}"
+  -d "{\"environment\": \"dev\", \"environment_url\": \"http://example.com\", \"state\": \"in_progress\", \"log_url\": \"https://github.com/$3/actions/runs/$6\"}"
 
 curl \
   -X POST \
-  -H "Content-Type: application/json,application/vnd.github.ant-man-preview+json" \
+  -H "Content-Type: application/json" \
   -H "Authorization: token $1" \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github.v3+json,application/vnd.github.ant-man-preview+json" \
   https://api.github.com/repos/"$3"/deployments/"$deployment_id"/statuses \
-  -d "{environment: 'dev', environment_url: 'http://example.com', state: 'success', log_url: 'https://github.com/$3/actions/runs/$6'}"
+  -d "{\"environment\": \"dev\", \"environment_url\": \"http://example.com\", \"state\": \"success\", \"log_url\": \"https://github.com/$3/actions/runs/$6\"}"
+
+echo "$GITHUB_REF"
