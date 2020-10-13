3f71439a9c7220ea00bdc75afcdecbfe58af3b0b
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 21:29:33 +0200

    Fixed jq

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -l
+#!/bin/bash
 
 result=$(curl -H "Authorization: token $1" -H "Accept: application/vnd.github.v3.full+json" \
  https://api.github.com/repos/$3/pulls/$4)
 
-jq '.head.ref' result
+jq '.head.ref' <<< "$result"
