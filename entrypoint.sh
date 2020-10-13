72146095ec85b06a947a429c76df0b1afe875774
Felix Kleinekathoefer <git@felixoi.com>
2020-10-14 00:32:02 +0200

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-result=$(curl -H "Authorization: token $1" -H "Accept: application/vnd.github.v3.full+json" \
- https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$4")
+pull_request_id=$(echo "$GITHUB_REF" | awk -F / '{print $3}')
 
+# use PAT if no github token is set
+if [ -z "$1" ]
+then
+      "$1"="$2"
+fi
+
+# fetch pull request branch
+result=$(curl -H "Authorization: token $1" -H "Accept: application/vnd.github.v3.full+json" \
+ https://api.github.com/repos/"$GITHUB_REPOSITORY"/pulls/"$pull_request_id")
 branch=$(echo "$result" | jq '.head.ref')
 
+# create deployment
 result2=$(curl \
   -X POST \
   -H "Content-Type: application/json" \
@@ -12,9 +21,9 @@ result2=$(curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/"$GITHUB_REPOSITORY"/deployments \
   -d "{\"ref\":$branch, \"environment\":\"dev\", \"required_contexts\": [], \"auto_merge\": false}")
-
 deployment_id=$(echo "$result2" | jq '.id')
 
+# create deployment status in_progress
 curl \
   -X POST \
   -H "Content-Type: application/json" \
@@ -23,6 +32,7 @@ curl \
   https://api.github.com/repos/"$GITHUB_REPOSITORY"/deployments/"$deployment_id"/statuses \
   -d "{\"environment\": \"dev\", \"environment_url\": \"http://example.com\", \"state\": \"in_progress\", \"log_url\": \"https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID\"}"
 
+# create deployment status success
 curl \
   -X POST \
   -H "Content-Type: application/json" \
