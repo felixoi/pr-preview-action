b6d4a5c4accab6bd3ed3ba36b2c37add4b0ab85a
Felix Kleinekathoefer <git@felixoi.com>
2020-10-17 14:03:43 +0200

    Request python

--- scripts/create_deployment.py
+++ scripts/create_deployment.py
@@ -3,7 +3,18 @@
 import json
 import os
 
-force_pat = os.getenv('INPUT_FORCE_PAT', 'false').lower() == 'true'
+import requests
 
-if force_pat:
-    print("FORCEEE")
+token = os.environ['INPUT_PA_TOKEN'] if os.getenv('INPUT_FORCE_PAT', 'false').lower() == 'true' \
+    else os.environ['GITHUB_TOKEN']
+headers = {
+    'Authorization': 'token ' + token,
+    'Accept': 'application/vnd.github.v3.full+json'
+}
+pr = os.environ['GITHUB_REF'].split('/')[2]
+repo = os.environ['GITHUB_REPOSITORY']
+
+requests.get(f'https://api.github.com/repos/{repo}/pulls/{pr}', headers)
+r = requests.get('%s/pulls/%s/files' % (repo, pr), headers)
+r.raise_for_status()
+print(r.json()['head']['ref'])
