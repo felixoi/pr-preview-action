d94ebada934b9408674c6994139cb4d02fc96684
Felix Kleinekathoefer <git@felixoi.com>
2020-10-18 18:42:08 +0200

    Something is wrong

--- scripts/deployment_create.py
+++ scripts/deployment_create.py
@@ -37,16 +37,14 @@ headers_custom = headers.copy()
 headers_custom['Authorization'] = 'token ' + os.environ['INPUT_GITHUB_TOKEN']
 headers_custom['Accept'] = "application/vnd.github.v3+json,application/vnd.github.ant-man-preview+json," \
                            "application/vnd.github.flash-preview+json"
-print(headers_custom)
 r = requests.post(f'{github_api}/repos/{repo}/deployments/{deployment_id}/statuses',
                   json.dumps({
                       "environment": f"PR-{pr}",
                       "environment_url": f"{pages_base}/{pr}",
                       "state": "in_progress",
                       "log_url": f"{github_base}/{repo}/actions/runs/{run_id}"
-                  }), headers_custom)
+                  }), headers=headers_custom)
 r.raise_for_status()
 
 pr = os.environ['DEPLOYMENT_ID'] = f"{deployment_id}"
 
-print("HELLLOOOO")
